50c0f9a080d8fe26a2a98203fd9f2731b75d0ef4
mahad002 <mahad112002@gmail.com>
2025-04-02 00:43:24 +0500

    ..

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import SUVModelsShowcase from '../components/SUVModelsShowcase';
-import FeaturedModels from '../components/FeaturedModels';
+// import FeaturedModels from '../components/FeaturedModels';
 import CTASection from '../components/CTASection';
 
 const Home = () => {
@@ -21,7 +21,7 @@ const Home = () => {
         </div>
         <SUVModelsShowcase />
       </div>
-      <FeaturedModels />
+      {/* <FeaturedModels /> */}
       <CTASection />
     </div>
   );
